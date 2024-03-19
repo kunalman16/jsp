4be771beba5d803eab210b247ebba599eb1b2997
kunalman16 <kunalmandal.367984@gmail.com>
2024-03-19 14:27:50 +0530

    javscriptpractise

---
+++ prc.js
@@ -0,0 +1,37 @@
+let student1 = {
+    name:"Kunal",
+    age: 19
+    // class: "12th"
+    // year of addmission: 2009
+    // marks obtained last year: 90%
+}
+// let student2 = {
+//     name:"Suhani",
+//     age: 18
+//     class: "12th"
+//     year of addmission: 2010
+//     marks obtained last year: 80%
+// }
+// let student3 = {
+//     name:"Vansh",
+//     age: 18
+//     class: "12th"
+//     year of addmission: 2020
+//     marks obtained last year: 55%
+// }
+// let student4 = {
+//     name:"Aditya",
+//     age: 19
+//     class: "12th"
+//     year of addmission: 2022
+//     marks obtained last year: 64%
+// }
+// let student5 = {
+//     name:"Clinton",
+//     age: 19
+//     class: "12th"
+//     year of addmission: 2007
+//     marks obtained last year: 1%
+// }
+
+console.log(user1)
